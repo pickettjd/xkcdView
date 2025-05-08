ae39b485d71deb5f1b89429d49cb0284ebc0b9fe
Jeff Pickett <jeff.pickett@gmail.com>
2025-05-08 11:57:47 -0400

    Added network manager

---
+++ xkcdView/xkcdView/View/NoNetworkView.swift
@@ -0,0 +1,18 @@
+//
+//  NoNetworkView.swift
+//  xkcdView
+//
+//  Created by Jeff Pickett on 5/8/25.
+//
+
+import SwiftUI
+
+struct NoNetworkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoNetworkView()
+}
